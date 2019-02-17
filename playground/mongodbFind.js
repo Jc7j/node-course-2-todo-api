cdaa1d82e1ac1e28d181939665be18685fc0c000
Cat Front Page <chiangjason19@gmail.com>
2019-02-16 20:52:53 -0800

    Initial commit

---
+++ playground/mongodbFind.js
@@ -0,0 +1,38 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {
+  MongoClient,
+  ObjectID
+} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', {
+  useNewUrlParser: true
+}, (err, client) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+  const db = client.db('TodoApp')
+
+  //Fetching within the query Todos, only get those who are completed false
+  // db.collection('Todos').find({
+  //   _id: new ObjectID('5c68e6b8a0e3af06f29f49c9')
+  // }).toArray().then((docs) => {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
+
+  // db.collection('Todos').find().count().then((count) => {
+  //   console.log(`Todos count: ${count}`);
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
+
+  db.collection('Users').find({name: 'Jason'}).toArray().then((docs) => {
+    console.log(JSON.stringify(docs, undefined, 2));
+  });
+
+
+  // client.close();
+});
