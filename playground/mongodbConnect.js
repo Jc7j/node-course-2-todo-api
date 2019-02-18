92e73a108f06f7e4d1c1674d19efd65acc9f6096
Cat Front Page <chiangjason19@gmail.com>
2019-02-17 17:27:42 -0800

    Add GET /todos route

--- playground/mongodbConnect.js
+++ playground/mongodbConnect.js
@@ -8,31 +8,31 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', {useNewUrlParser: true}
     console.log('Connected to MongoDB server');
     const db = client.db('TodoApp')
 
-    // // Add new doc into Todos doc
-    // db.collection('Todos').insertOne({
-    //     text: 'Bread money',
-    //     completed: true
-    // }, (err, result) => {
-    //     if (err) {
-    //         return console.log('Unable to insert todo', err);
-    //     }
-
-    //     console.log(JSON.stringify(result.ops, undefined, 2)); // ops is going to store all of the docs that were inserted in this case
-    // });
-
-    // Insert new doc into Users (name, age, location)
-    db.collection('Users').insertOne({
-        name: 'Jason',
-        age: 21,
-        location: 'Las Vegas'
+    // Add new doc into Todos doc
+    db.collection('Todos').insertOne({
+        text: 'Bread money',
+        completed: true
     }, (err, result) => {
         if (err) {
-            return console.log('Unable to insert user', err);
+            return console.log('Unable to insert todo', err);
         }
 
-        console.log(result.ops[0]._id.getTimestamp());
-        console.log(JSON.stringify(result.ops, undefined, 2));
+        console.log(JSON.stringify(result.ops, undefined, 2)); // ops is going to store all of the docs that were inserted in this case
     });
+
+    // // Insert new doc into Users (name, age, location)
+    // db.collection('Users').insertOne({
+    //     name: 'Jason',
+    //     age: 21,
+    //     location: 'Las Vegas'
+    // }, (err, result) => {
+    //     if (err) {
+    //         return console.log('Unable to insert user', err);
+    //     }
+
+    //     console.log(result.ops[0]._id.getTimestamp());
+    //     console.log(JSON.stringify(result.ops, undefined, 2));
+    // });
 
     client.close();
 });
