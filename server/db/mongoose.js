a09a5cefa665673efa7dc2de16f12ca0d626d414
Cat Front Page <chiangjason19@gmail.com>
2019-02-18 14:20:57 -0800

    Added useNewUrlParser into mongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose'); 
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.event.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', {useNewUrlParser: true});
+mongoose.connect(process.event.MONGODB_URI, {useNewUrlParser: true} || 'mongodb://localhost:27017/TodoApp', {useNewUrlParser: true});
 
 module.exports = {mongoose};
